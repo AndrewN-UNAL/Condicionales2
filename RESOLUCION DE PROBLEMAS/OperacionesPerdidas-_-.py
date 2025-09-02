467351a6ced035a1974f6245250c4651ae20129a
bob@example.com <bob@example.com>
2025-09-01 22:54:57 -0500

    Adicion de las operaciones

--- RESOLUCION DE PROBLEMAS/OperacionesPerdidas-_-.py
+++ RESOLUCION DE PROBLEMAS/OperacionesPerdidas-_-.py
@@ -5,7 +5,7 @@
 
 print("hola, ya puedo imprimir frases")
 
-S = 5
+S = 6
 
 #concersiones entre int, float y str
 
@@ -18,3 +18,33 @@ print("ahora es un decimal",type(S), S)
 S = str(S)
 
 print("y ahora es un texto",type(S), S)
+
+S = int(S)
+
+#Operaciones
+
+print("seguimos con las operaciones básicas")
+
+#suma
+
+print("Esta es la suma", S+5)
+
+#resta
+
+print("Esta es la resta", S-5)
+
+#multiplicación
+
+print("Esta es la multiplicación", S*5)
+
+#división
+S = float(S)
+print("Esta es la divisón", S/5)
+S = int(S)
+#potencia
+
+print("Esta es la potencia", S**5)
+
+#Raiz
+S = float(S)
+print("Y Esta es la Raiz", S**(1/2))
