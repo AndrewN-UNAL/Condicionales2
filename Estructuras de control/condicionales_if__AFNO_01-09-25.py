32f1bca12736c4e2675b7b3b5303dc47f6f91cab
bob@example.com <bob@example.com>
2025-09-01 22:01:02 -0500

    Condicional if y valor verdadero

--- Estructuras de control/condicionales_if__AFNO_01-09-25.py
+++ Estructuras de control/condicionales_if__AFNO_01-09-25.py
@@ -7,3 +7,10 @@ print(type(A), "y la variable valor", A)
 
 
 #Estructuras de control
+
+#Condicional if y valor verdadero
+
+if(A>1):
+    print("este es el valor del If verdadero")
+    print(f"A es mayor que 1 y suvalor es {A}")
+
