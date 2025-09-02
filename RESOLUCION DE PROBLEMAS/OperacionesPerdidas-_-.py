a2deb0fa62d8afde7b236d795e0d7363b80b53aa
bob@example.com <bob@example.com>
2025-09-01 22:44:36 -0500

    conversiones entre typos de datos

--- RESOLUCION DE PROBLEMAS/OperacionesPerdidas-_-.py
+++ RESOLUCION DE PROBLEMAS/OperacionesPerdidas-_-.py
@@ -2,3 +2,15 @@
 #Fecha: 2023-09-01
 
 #Operaciones simples y print
+
+print("hola, ya puedo imprimir frases")
+
+S = 5
+
+#concersiones entre int, float y str
+
+print("te doy un entero",type(S), int(S))
+
+print("ahora es un decimal",type(S), float(S))
+
+print("y ahora es un texto",type(S), str(S))
